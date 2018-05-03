52c3d04c306842f37b942ff7456394ae55736379
Tomasz <tlubowiecki@jakku.fritz.box>
2018-05-03 16:00:00 +0200

    Usereingaben verarbeiten

--- ScoutVerwaltung/src/de/gfn/oca/scoutcamp/App.java
+++ ScoutVerwaltung/src/de/gfn/oca/scoutcamp/App.java
@@ -1,6 +1,9 @@
 package de.gfn.oca.scoutcamp;
 
 import de.gfn.oca.scoutcamp.entity.Scout;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -8,18 +11,51 @@ import de.gfn.oca.scoutcamp.entity.Scout;
  */
 public class App {
     
+    private List<Scout> scouts = new ArrayList<>();
+    
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
+        
         System.out.println("START");
+        
+        App app = new App();
+        
         //TODO: Scouts aus Datenbank oder Datei lesen
         
         // User-Eingabe verarbeiten
         
-        Scout s1 = new Scout("Peter", "Parker");
+        boolean exit = false;
         
-        Scout s2 = new Scout();
-        s2.setFirstname("Bruce");
-        s2.setLastname("Wayne");
+        while(!exit) {
+            
+            switch(scanner.next()) {
+                
+                case "N": System.out.println("New Scout"); 
+                    break;
+                    
+                case "L": System.out.println("List of Scout"); 
+                    break;
+                    
+                case "X": System.out.println("Exit");
+                    exit = true;
+                    break;
+            }
+            
+            
+            
+            /*
+            String firstname = scanner.next();
+            String lastname = scanner.next();
+            
+            if(firstname.equalsIgnoreCase("exit"))
+                break;
+            
+            Scout s = new Scout(firstname, lastname);
+            app.scouts.add(s);
+            System.out.println(s);
+            */
+        }
         
         //TODO: Scouts in die Datenbank oder Datei schreiben
         System.out.println("END");
